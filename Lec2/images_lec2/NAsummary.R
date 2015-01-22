bb92fb68eb3dd9365f436005f82ba80c37445530
Gene Leynes <gleynes@gmail.com>
2015-01-22 07:09:52 -0600

    Lec 2 changes, and lecture_plan.xlsx

---
+++ Lec2/images_lec2/NAsummary.R
@@ -0,0 +1,34 @@
+#' @name   NAsummary
+#' @title  Summarize NA values in a matrix or data.frame (or data.table)
+#' @author Gene Leynes
+#' 
+#' @param df            A data.frame to be summarized
+#' @param include_nan   Do you want to also see Nan's along with NA's
+#'                      Defaults to FALSE
+#'
+#' @description
+#' 		Summarize the available information in a data.frame (or similar) quickly
+
+NAsummary <-function(df, include_nan=FALSE){
+	newdf = data.frame(col=1:ncol(df),
+					   Count =nrow(df),
+					   nNA = sapply(df,function(x)length(x[is.na(x)])))
+	
+	newdf$rNA = newdf$nNA / newdf$Count
+	newdf$rNA = trunc(newdf$rNA*10000)/10000
+	
+	if(include_nan){
+		newdf$nNan = sapply(df,function(x)length(x[is.nan(x)]))
+		newdf$rNan = newdf$nNan / newdf$Count
+		newdf$rNan = trunc(newdf$rNan*10000)/10000
+	}
+	
+	newdf$nUnique = sapply(df,function(x)length(unique(x)))
+	
+	newdf$rUnique = newdf$nUnique / newdf$Count
+	newdf$rUnique = trunc(newdf$rUnique*10000)/10000
+	
+	rownames(newdf) = colnames(df)
+	return(newdf)
+}
+
